e4408a43594450aefe3ea456ff5740d6d59adc62
Leandro Diomar <leandrodiomar123@gmail.com>
2022-03-25 02:20:36 -0300

    Atualizacao api Users

--- triAppBack/src/main/java/Application/TriAppApplication.java
+++ triAppBack/src/main/java/Application/TriAppApplication.java
@@ -14,6 +14,7 @@ public class TriAppApplication {
 		return "Application Started";
 	}
 	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TriAppApplication.class, args);
 	}
--- triAppBack/src/main/java/Application/rest/controller/UserController.java
+++ triAppBack/src/main/java/Application/rest/controller/UserController.java
@@ -1,15 +1,18 @@
 package Application.rest.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
 
 
 import Application.domain.entity.User;
 import Application.domain.repository.Users;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -29,4 +32,60 @@ public class UserController {
 
         return userSalvo;
     }
+
+	@GetMapping(
+			value = {"{id}"}
+	)
+	public User getUserById(@PathVariable Integer id){
+
+		return repository
+				.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Cliente nao encontrado"));
+
+	}
+
+	@DeleteMapping(
+			value = {"{id}"}
+	)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete( @PathVariable Integer id){
+		repository.findById(id)
+				.map(user -> {
+					repository.delete(user);
+					return user;
+				})
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+						"Usuário não encontrado"));
+
+	}
+
+	@PutMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void  update(@PathVariable Integer id,
+						@RequestBody @Valid User user){
+		repository
+				.findById(id)
+				.map(clienteExistente ->{
+					user.setId(clienteExistente.getId());
+					repository.save(user);
+					return ResponseEntity.noContent().build();
+				}).orElseThrow( ()-> new ResponseStatusException(HttpStatus.NOT_FOUND,
+						"Usuario não encontrado"));
+	}
+
+	@GetMapping()
+	public List<User> find(User filtro){
+
+		ExampleMatcher matcher = ExampleMatcher
+				.matching()
+				.withIgnoreCase()
+				.withStringMatcher(ExampleMatcher
+						.StringMatcher.CONTAINING);
+
+		Example example = Example.of(filtro, matcher);
+
+		return repository.findAll(example);
+
+	}
+
 }
